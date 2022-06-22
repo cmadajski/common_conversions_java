8b1aac142068925de88a34676c61735cf90f5547
Christian Madajski <cmad7317@tutanota.com>
2022-06-22 10:30:15 -0400

    Add error message for incorrect args

--- src/Driver.java
+++ src/Driver.java
@@ -6,8 +6,13 @@ class Driver {
 			System.out.println("** No args provided. Use [VALUE] [FROM_UNIT] [TO_UNIT] to process data. **");
 		} else {
 			Conversion newConv = new Conversion(Double.parseDouble(args[0]), args[1], args[2]);
-			System.out.printf("%,.2f %s is %,.2f %s", newConv.getFromValue(), newConv.getFromUnit(),
-					newConv.getToValue(), newConv.getToUnit());
+			// print error message if getToUnit() returns -1
+			if (newConv.getToValue() == -1) {
+				System.out.printf("ERROR: Can't convert from %s to %s", newConv.getFromUnit(), newConv.getToUnit());
+			} else {
+				System.out.printf("%,.2f %s is %,.2f %s", newConv.getFromValue(), newConv.getFromUnit(),
+						newConv.getToValue(), newConv.getToUnit());
+			}
 		}
 	}
 }
